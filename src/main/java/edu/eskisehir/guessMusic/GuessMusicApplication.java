da9ec1e14bce29b21ececfecd1bef85e68d1975c
lKhanl <oguzhanercelik@eskisehir.edu.tr>
2022-10-13 00:39:33 +0300

    init

---
+++ src/main/java/edu/eskisehir/guessMusic/GuessMusicApplication.java
@@ -0,0 +1,13 @@
+package edu.eskisehir.guessMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eskisehir/guessMusic/GuessMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eskisehir.guessMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
