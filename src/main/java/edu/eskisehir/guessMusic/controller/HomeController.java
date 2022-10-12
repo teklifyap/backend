6c70d2e58b74ce7b248e812ee365a7c4818a8351
lKhanl <oguzhanercelik@eskisehir.edu.tr>
2022-10-13 00:39:33 +0300

    init

---
+++ src/main/java/edu/eskisehir/guessMusic/controller/HomeController.java
@@ -0,0 +1,16 @@
+package edu.eskisehir.guessMusic.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+@AllArgsConstructor
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "Guess Music";
+    }
+
+}
