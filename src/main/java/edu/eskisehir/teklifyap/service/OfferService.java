7ce2b2923bf3b3456e69dda1711f5d6122e67b31
osmancandogan <osmancandogan@eskisehir.edu.tr>
2022-11-02 23:52:03 +0300

    Controller and Service Methods are created. Annotations corrected.

--- src/main/java/edu/eskisehir/teklifyap/service/OfferService.java
+++ src/main/java/edu/eskisehir/teklifyap/service/OfferService.java
@@ -1,4 +1,28 @@
 package edu.eskisehir.teklifyap.service;
 
+import edu.eskisehir.teklifyap.domain.dto.OfferDto;
+import edu.eskisehir.teklifyap.domain.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OfferService {
+    public void createOffer(OfferDto offerDto, User user) {
+
+    }
+
+    public String getOffers(User user) {
+        return null;
+    }
+
+    public void deleteOffer(Long id, User user) {
+
+    }
+
+    public void updateOffer(OfferDto offerDto, User user) {
+
+    }
+
+    public String getOffer(Long id, User user) {
+        return null;
+    }
 }
--- src/main/java/edu/eskisehir/teklifyap/service/WorksiteService.java
+++ src/main/java/edu/eskisehir/teklifyap/service/WorksiteService.java
@@ -1,4 +1,29 @@
 package edu.eskisehir.teklifyap.service;
 
+import edu.eskisehir.teklifyap.domain.dto.WorksiteDto;
+import edu.eskisehir.teklifyap.domain.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public class WorksiteService {
+    public void createWorksite(WorksiteDto worksiteDto, User user) {
+
+    }
+
+    public String getWorksites(User user) {
+        return null;
+    }
+
+    public void deleteWorksite(Long id, User user) {
+
+    }
+
+    public void updateWorksite(WorksiteDto worksiteDto, User user) {
+
+
+    }
+
+    public String getWorksite(Long id, User user) {
+        return null;
+    }
 }
