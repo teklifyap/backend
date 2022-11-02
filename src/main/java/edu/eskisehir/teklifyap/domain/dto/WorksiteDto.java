df6b0446389efede2019f8148d82821077152d58
osmancandogan <osmancandogan@eskisehir.edu.tr>
2022-11-02 23:19:35 +0300

    all relations added. Dto classes created.

---
+++ src/main/java/edu/eskisehir/teklifyap/domain/dto/WorksiteDto.java
@@ -0,0 +1,9 @@
+package edu.eskisehir.teklifyap.domain.dto;
+
+public class WorksiteDto {
+    private Long id;
+    private String name;
+    private String address;
+    private double locationX;
+    private double locationY;
+}
