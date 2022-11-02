c59c9e4ca81eae1a5289aa1061bff691b9b07dfe
osmancandogan <osmancandogan@eskisehir.edu.tr>
2022-11-02 23:34:43 +0300

    AOP layers created, item controller and service added.

---
+++ src/main/java/edu/eskisehir/teklifyap/controller/WorksiteController.java
@@ -0,0 +1,4 @@
+package edu.eskisehir.teklifyap.controller;
+
+public class WorksiteController {
+}
---
+++ src/main/java/edu/eskisehir/teklifyap/repository/OfferRepository.java
@@ -0,0 +1,9 @@
+package edu.eskisehir.teklifyap.repository;
+
+import edu.eskisehir.teklifyap.domain.model.Offer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OfferRepository extends JpaRepository<Offer,Long> {
+}
