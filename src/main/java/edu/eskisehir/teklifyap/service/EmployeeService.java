7232af4f88a9c1eb399c0a196b2ed95c51411c9b
osmancandogan <osmancandogan@eskisehir.edu.tr>
2022-11-06 19:36:46 +0300

    EmployeeService and WorksiteService coded.

--- src/main/java/edu/eskisehir/teklifyap/service/EmployeeService.java
+++ src/main/java/edu/eskisehir/teklifyap/service/EmployeeService.java
@@ -1,28 +1,60 @@
 package edu.eskisehir.teklifyap.service;
 
 import edu.eskisehir.teklifyap.domain.dto.EmployeeDto;
+import edu.eskisehir.teklifyap.domain.model.Employee;
 import edu.eskisehir.teklifyap.domain.model.User;
+import edu.eskisehir.teklifyap.repository.EmployeeRepository;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeService {
-    public void createEmployee(EmployeeDto employeeDto, User user) {
 
+    EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
-    public String getEmployees(User user) {
-        return null;
+    public EmployeeDto createEmployee(EmployeeDto employeeDto, User user) {
+        Employee employee = Employee.builder()
+                .name(employeeDto.getName())
+                .surname(employeeDto.getSurname())
+                .user(user)
+                .build();
+        return EmployeeDto.builder()
+                .id(employeeRepository.save(employee).getId())
+                .name(employee.getName())
+                .surname(employee.getSurname())
+                .build();
+    }
+    public String getEmployees() {
+        return employeeRepository.findAll().stream().map(employee -> EmployeeDto.builder()
+                .id(employee.getId())
+                .name(employee.getName())
+                .surname(employee.getSurname())
+                .build()).toList().toString();
     }
 
     public void deleteEmployee(Long id, User user) {
-
+        employeeRepository.deleteById(id);
     }
 
     public void updateEmployee(EmployeeDto employeeDto, User user) {
-
+    employeeRepository.findById(employeeDto.getId()).ifPresent(employee -> {
+        employee.setName(employeeDto.getName());
+        employee.setSurname(employeeDto.getSurname());
+        employeeRepository.save(employee);
+    });
     }
 
     public String getEmployee(Long id, User user) {
-        return null;
+        return employeeRepository.findById(id).map(employee -> EmployeeDto.builder()
+                .id(employee.getId())
+                .name(employee.getName())
+                .surname(employee.getSurname())
+                .build()).toString();
     }
 }
--- src/main/java/edu/eskisehir/teklifyap/service/WorksiteService.java
+++ src/main/java/edu/eskisehir/teklifyap/service/WorksiteService.java
@@ -2,28 +2,59 @@ package edu.eskisehir.teklifyap.service;
 
 import edu.eskisehir.teklifyap.domain.dto.WorksiteDto;
 import edu.eskisehir.teklifyap.domain.model.User;
+import edu.eskisehir.teklifyap.domain.model.Worksite;
+import edu.eskisehir.teklifyap.repository.WorksiteRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WorksiteService {
-    public void createWorksite(WorksiteDto worksiteDto, User user) {
+    WorksiteRepository worksiteRepository;
 
+    public WorksiteService(WorksiteRepository worksiteRepository) {
+        this.worksiteRepository = worksiteRepository;
+    }
+
+    public void createWorksite(WorksiteDto worksiteDto, User user) {
+        Worksite worksite = Worksite.builder()
+                .name(worksiteDto.getName())
+                .address(worksiteDto.getAddress())
+                .locationX(worksiteDto.getLocationX())
+                .locationY(worksiteDto.getLocationY())
+                .build();
+        worksite=worksiteRepository.save(worksite);
     }
 
     public String getWorksites(User user) {
-        return null;
+        return worksiteRepository.findAll().stream().map(worksite -> WorksiteDto.builder()
+                .id(worksite.getId())
+                .name(worksite.getName())
+                .address(worksite.getAddress())
+                .locationX(worksite.getLocationX())
+                .locationY(worksite.getLocationY())
+                .build()).toList().toString();
     }
 
     public void deleteWorksite(Long id, User user) {
-
+        worksiteRepository.deleteById(id);
     }
 
     public void updateWorksite(WorksiteDto worksiteDto, User user) {
-
-
+        worksiteRepository.findById(worksiteDto.getId()).ifPresent(worksite -> {
+            worksite.setName(worksiteDto.getName());
+            worksite.setAddress(worksiteDto.getAddress());
+            worksite.setLocationX(worksiteDto.getLocationX());
+            worksite.setLocationY(worksiteDto.getLocationY());
+            worksiteRepository.save(worksite);
+        });
     }
 
     public String getWorksite(Long id, User user) {
-        return null;
+        return worksiteRepository.findById(id).map(worksite -> WorksiteDto.builder()
+                .id(worksite.getId())
+                .name(worksite.getName())
+                .address(worksite.getAddress())
+                .locationX(worksite.getLocationX())
+                .locationY(worksite.getLocationY())
+                .build()).toString();
     }
 }
