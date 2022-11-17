0d8304d1b37a9478808de18e717687e7d5b7004d
Khan <oguzhanercelik@eskisehir.edu.tr>
2022-11-18 02:57:42 +0300

    test

--- src/main/java/edu/eskisehir/teklifyap/TeklifyapApplication.java
+++ src/main/java/edu/eskisehir/teklifyap/TeklifyapApplication.java
@@ -11,7 +11,6 @@ public class TeklifyapApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TeklifyapApplication.class, args);
 		log.info("Application is just started now!");
-
 	}
 
 }
