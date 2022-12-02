e7288a65e9a430f26d07560f46dde22bbc624ac0
OCD <osmancandogan@eskisehir.edu.tr>
2022-12-03 00:00:25 +0300

    EmployeeRowDataDto.java added

--- src/main/java/edu/eskisehir/teklifyap/domain/dto/EmployeeRowDataDto.java
+++ src/main/java/edu/eskisehir/teklifyap/domain/dto/EmployeeRowDataDto.java
@@ -2,13 +2,17 @@ package edu.eskisehir.teklifyap.domain.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-public class EmployeeNameDto {
+@NoArgsConstructor
+public class EmployeeRowDataDto {
 
     private int no;
     private String name;
     private String surname;
+    private double salary;
+
 
 }
